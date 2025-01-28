db79292818bca024f647615d7f9ca54da624217c
Almas <almasemilius@gmail.com>
2025-01-28 09:41:24 +0300

    adding service file

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ SERVER_HOST = "0.0.0.0"  # Listen on all network interfaces
 SERVER_PORT = 8081       # The port your locks are configured to connect to
 
 # Security
-SECRET_KEY = "your_secret_key"  # Replace with the secret key configured in your locks
+SECRET_KEY = "your_secret_key"  # IMPORTANT: Change this to a strong secret key!
 
 # Logging
 LOG_LEVEL = "INFO"
-LOG_FILE = "bike_locks.log" 
+LOG_FILE = "/var/log/bike-lock-server.log"  # Changed to system log directory 
